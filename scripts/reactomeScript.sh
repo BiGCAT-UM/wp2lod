5926a8ff4335215478d34b0cabe58b801882a387
Egon Willighagen <egon.willighagen@gmail.com>
2014-11-30 10:30:49 +0100

    Tuned for Jenkins

--- scripts/reactomeScript.sh
+++ scripts/reactomeScript.sh
@@ -1,26 +1,20 @@
 rm -rf /tmp/GPML
 mkdir /tmp/GPML
-rm -rf /tmp/OPSWPRDF
-mkdir /tmp/OPSWPRDF
-rm -rf /tmp/OPSWIKIDUMP
-mkdir /tmp/OPSWIKIDUMP
+rm -rf /tmp/WPREACTRDF
+mkdir /tmp/WPREACTRDF
+rm -rf /tmp/OPSREACTOMEDUMP
+mkdir /tmp/OPSREACTOMEDUMP
 cp ../BioDataSource.ttl /tmp
-wget -O /tmp/OPSWIKIDUMP/reactome.zip "http://www.wikipathways.org//wpi/batchDownload.php?species=Homo%20sapiens&fileType=gpml&tag=Curation:Reactome_Approved"
-cd /tmp/OPSWIKIDUMP
+wget -O /tmp/OPSREACTOMEDUMP/reactome.zip "http://www.wikipathways.org//wpi/batchDownload.php?species=Homo%20sapiens&fileType=gpml&tag=Curation:Reactome_Approved"
+cd /tmp/OPSREACTOMEDUMP
 for file in $(ls); do unzip $file; done
 rm *.zip
 cd /tmp
 
 # Generate the triples
 rm /tmp/wpContent*
- java -Xmx4G -jar WP2Rdf.jar
+java -Xmx4G -jar WP2Rdf.jar
 
-#Empty the triplestore
-# isql-vt -U dba -P <PASSWORD> -S <PORT> "EXEC=RDF_GLOBAL_RESET ();"
-
-export TTLFILE=`ls /tmp/wpContent*`
-
-#Fill the triples store with the obtained triple
-
-# infer the biological relations
-# java -jar /tmp/inferDirection.jar 19755
+cd "${WORKSPACE}"
+cd ..
+ant run
